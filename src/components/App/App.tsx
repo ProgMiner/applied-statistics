a0e85ab6f979be58087759b8a83cc7f33e8576ef
Eridan Domoratskiy <eridan200@mail.ru>
2020-01-09 22:30:33 +0300

    Add welcome widget

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,12 +4,26 @@ import { Week6 } from '../../weeks/Week6/Week6';
 import { Week7 } from '../../weeks/Week7/Week7';
 
 import './App.css';
+import { Panel } from 'primereact/panel';
 
 export class App extends React.Component {
 
     render() {
         return (
             <div className="App">
+                <Panel header="Добро пожаловать!">
+                    Это онлайн программа для решения задач из упражнений онлайн-курса Прикладная статистика.
+                    <br />
+
+                    Автор:&nbsp;
+                    <a href="https://byprogminer.ru/vk" target="_blank" rel="noopener noreferrer">Доморацкий Эридан</a>
+                    <br />
+
+                    <a href="https://my.qiwi.com/Erydan-DMVuYwfjtW" target="_blank" rel="noopener noreferrer">
+                        <small>(Донатить сюда)</small>
+                    </a>
+                </Panel>
+
                 <Week6 />
                 <Week7 />
             </div>
